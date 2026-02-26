3cd640bc9111f5c3f4c163e142ad30aff9602626
Siri <bsiri@2030.com>
2026-02-26 13:14:21 +0530

    bug in register.jsx

--- FreeRein/src/pages/Register.jsx
+++ FreeRein/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import './Auth.css';
+
 
 const Register = () => {
   const [formData, setFormData] = useState({ name: '', email: '', password: '' });
